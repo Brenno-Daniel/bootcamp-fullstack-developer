1a600ff35e33c9df5f82f8b27c66a055558aa015
Brenno Daniel <brenno.daniel.98@gmail.com>
2020-07-01 20:44:53 -0300

    adding course 20

---
+++ 20 - Desenvolvimento_avancado_com_Javascript_ES6/1-Funcoes_avancadas_do_ES6/1-Arrow-Function.js
@@ -0,0 +1,83 @@
+// Parte 1: Arrow Functions
+
+var sumOld = function(a, b){
+    return a + b;
+}
+
+
+// Arrow functions
+// São funções anonimas
+
+var sum = (a, b) => a + b; // existe um return implicito
+
+console.log(sum(5, 15));
+
+// se possuir um argumento podemos omitir os parenteses
+
+var sum = a => a + 5;
+
+// objetos literais
+
+var obj = {
+    teste: '123'
+}
+
+// return implicito de um objeto
+
+var createObj = () => ({ test: 123 });
+
+console.log(createObj());
+
+// outra forma de construir objetos é através de funções construtoras
+
+// function Car() {
+//     this.foo = 'bar';
+// }
+
+// console.log(new Car());
+
+// não podemos fazer o mesmo utilizando arrow function, por conta do this (veremos mais a frente)
+
+var Car = () => {
+    this.foo = 'bar';
+};
+
+console.log(new Car());
+
+// hoisting: caracteristicas das funções e variaveis de serem escaladas ao topo do nosso código
+// consigo invocar a função antes de declarar a variavel
+
+log ('teste');
+
+function log(value) {
+    console.log(value);
+}
+
+// conceito de hoisting não se aplica a arrow function
+
+log('teste');
+
+var log = value => {
+    console.log(value);
+};
+
+//  *********
+
+var obj = {
+
+    showContext: function showContext() {
+        console.log(this);
+    },
+
+    log: function log(value) {
+        console.log(value);
+    }
+};
+
+obj.showContext();
+
+// this:
+// bind e aply fixam o contexto da função
+// comum nos anos 200 armazenar o contexto numa variavel, geralmente var _that = this; (deixava o codigo mais poluido, etc.)
+// arrow function veio para simplificar esse processo
+// tem uma propriedade de ter um contexto igual ao codigo que envolve ele
